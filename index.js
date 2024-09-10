b0529f72968d530e8872e1255b55fca165c1be63
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-09-10 20:14:24 +0100

    Add the router

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const http = require
+const http = require("http");
+const url = require("url");
+const fs = require("fs")
+http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if(parsedUrl.pathname === "/"){
+        fs.readFile("./index.html", (err, data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.end(data);
+        })
+    }
+    else if(parsedUrl.pathname === "/about"){
+        fs.readFile("./about.html", (err, data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.end(data);
+        })
+    }
+    else if(parsedUrl.pathname === "/contact"){
+        fs.readFile("./contact-me.html", (err, data)=>{
+            res.writeHead(200, {"Content-Type":"text/html"});
+            res.end(data);
+        })
+    }
+    else{
+        fs.readFile("./404.html", (err, data)=>{
+            res.writeHead(404, {"Content-Type":"text/html"});
+            res.end(data);
+        })
+    }
+}).listen(8000, ()=> console.log("Server Running on port 8000"));
